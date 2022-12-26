0d35c239e32dd96a022c106062c12873b862f3cb
FatemaAlbalushi <fatoomsaid188@gmail.com>
2022-12-26 01:31:21 -0800

    Add the Recurssion practice program

--- src/Stackpractice.java
+++ src/Stackpractice.java
@@ -9,31 +9,57 @@ import java.util.Stack;
  *
  */
 public class Stackpractice {
-
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String str="Fatma";
-		String str1 = "{int i=0;}{int x=1 {{}}}";	
-		System.out.println("Print the reverse string of "+ str);
+		String str = "Something";
+		//String str1 = "{int i=0;}{int x=1 {{}}}";
+		// String str1 = "{{int x=0;}}}";
+		String str1 = "{";
+		System.out.println("Print the reverse string of " + str);
 		reString(str);
+		System.out.println();
+		System.out.println("Print if there is open barantheses " + countBarn(str1));
 
 	}
+
 	public static void reString(String userString) {
-		
+
 		Stack<Character> mystack = new Stack<Character>();
-		for(int index = 0; index<userString.length();index++) {
+		for (int index = 0; index < userString.length(); index++) {
 			mystack.push(userString.charAt(index));
-			
+
 		}
 		while (!mystack.empty()) {
-			System.out.print(mystack.pop());	
-				
+			System.out.print(mystack.pop());
+
 		}
-		
-		
+	}
+
+	public static boolean countBarn(String str) {
+		Stack<Character> mystack1 = new Stack<Character>();
+
+		for (int index = 0; index < str.length(); index++) {
+
+			if (str.charAt(index) == '{') {
+				mystack1.push(str.charAt(index));
+			}
+
+			else if (str.charAt(index) == '}') {
+				if (mystack1.empty()) {
+					return false;
+					
+				} else {
+					mystack1.pop();
+				}
+
+			}
+
+		}
+
+		return true;
 	}
 
 }
